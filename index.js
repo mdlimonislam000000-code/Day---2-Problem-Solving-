1d01471bb82526e8c52dc624fb175a09c9efdbb9
Md Limon Mia <mdlimonislam000000@gmail.com>
2026-09-17 17:48:09 +0600

    problem -2 solved

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-// Problem - 1 
+// Problem - 6
 
 const reverseString = (str) =>{
     return str.split('').reverse().join('')
 }
 console.log('Problem - 6 Reverse  String:',reverseString('Limon'))
+
+// Problem - 7 
+
+const countVowels = (str)=>{
+    const matches = str.match(/[aeiou]/gi)
+    return matches ? matches.length : 0;
+}
+console.log('Problem - 6 Count Vowels:',countVowels('limon'))
